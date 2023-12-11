01738df6e8b9a75e0982ca2711e4a192d4eee3a9
bw0711 <bw0711@gmail.com>
2023-12-11 21:31:50 +0200

    added reporter

--- cypress.config.js
+++ cypress.config.js
@@ -34,7 +34,7 @@ module.exports = defineConfig({
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/results',
-    overwrite: false,
+    overwrite: true,
     html: true,
     json: true,
   },
