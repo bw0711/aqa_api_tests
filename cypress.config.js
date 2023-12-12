8d73ab8ec0f97b0c8a6c58167d1956f867f7b69d
bw0711 <bw0711@gmail.com>
2023-12-12 10:12:08 +0200

    hide projectId

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 require("dotenv").config();
 
 module.exports = defineConfig({
-  projectId: "arrr4h",
+  projectId: process.env.PROJECT_ID,
   e2e: {
     setupNodeEvents(on, config) {
       const environmentName = config.env.environmentName || 'bookcart'
